bbbb27e81f1fb80f734ba6602dd76d9ece46c0e4
John Shpati <johnshpati@gmail.com>
2020-07-24 21:03:42 -0400

    updates

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("C")
+        print(sender.titleLabel!.text)
+        
+        playSound()
     }
     
-    
-
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
